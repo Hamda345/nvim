55b3e7215714c5f1fe706c133f9ff79e91a32135
Hamda345 <hdallel13@gmail.com>
2024-12-09 02:26:21 +0100

    custom config

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Leader>-"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+          -- other existing mappings...
+        },
+      },
+    },
+  },
+}
